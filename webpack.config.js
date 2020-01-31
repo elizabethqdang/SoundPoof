1f94b0f46949b409cbcfcb3ab1b163b205a5b16f
eqdang <elizabethqdang@gmail.com>
2020-01-31 10:33:03 -0800

    prepare for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './frontend/rachetpoof.jsx',
-    output: {
-        filename: './bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: [/\.jsx?$/],
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['@babel/env', '@babel/react']
-                    }
-                },
-            }
-        ]
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.jsx', '*']
-    }
+  entry: "./frontend/rachetpoof.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "./bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
