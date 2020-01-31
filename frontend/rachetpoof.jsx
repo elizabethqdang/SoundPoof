6638536941651f04fd0523347aac3813dc888378
eqdang <elizabethqdang@gmail.com>
2020-01-31 09:15:08 -0800

    prepare for heroku

--- frontend/rachetpoof.jsx
+++ frontend/rachetpoof.jsx
@@ -1,8 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from "./store/store"
+// import Root from "./components/root"
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 //     const root = document.getElementById('root');
 //     const store = configureStore();
-//     ReactDOM.render(<Root store={store}, root />)
-// });
+//     const Launch = <h1>RachetPoof has launched!</h1>
+    ReactDOM.render(
+      <h2>RachetPoof has launched!</h2>,
+      document.getElementById("root")
+    );
+});
