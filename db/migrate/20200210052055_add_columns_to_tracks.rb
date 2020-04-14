63f58fc1c1f7a1110714e9ae294a1c79ccde0121
eqdang <elizabethqdang@gmail.com>
2020-04-13 21:53:20 -0700

    upload track w/ active storage

--- db/migrate/20200210052055_add_columns_to_tracks.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToTracks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tracks, :artwork_file, :string
-    add_column :tracks, :track_file, :string
-    add_column :tracks, :artwork_url, :string
-    add_column :tracks, :track_url, :string
-  end
-end
