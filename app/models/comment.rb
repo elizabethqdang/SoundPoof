2807e359f707395e6f7eb09a8f7664804ba0d9f0
eqdang <elizabethqdang@gmail.com>
2020-04-22 11:37:20 -0700

    db

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
   validates :body, :user, :track, presence: true
 
-	belongs_to :user
+	belongs_to :user,
 			foreign_key: :user_id,
 			class_name: :User,
 			primary_key: :id
-  belongs_to :track
+  belongs_to :track,
   		foreign_key: :track_id,
 			class_name: :Track,
 			primary_key: :id
