4e8b749855932079386c78ce6454bf10ac1e7785
eqdang <elizabethqdang@gmail.com>
2020-08-25 01:07:09 -0700

    reducer refactor

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,7 +3,6 @@ import sessionReducer from "./session_reducer";
 import errorsReducer from "./errors_reducer";
 import uiReducer from "./ui_reducer";
 import trackplayerReducer from "./trackplayer_reducer";
-// import searchReducer from "./search_reducer";
 import usersReducer from "./users_reducer";
 import tracksReducer from "./tracks_reducer";
 import commentsReducer from "./comments_reducer";
@@ -14,7 +13,6 @@ const rootReducer = combineReducers({
 	tracks: tracksReducer,
 	comments: commentsReducer,
 	trackplayer: trackplayerReducer,
-	// search: searchReducer,
 	errors: errorsReducer,
 	ui: uiReducer,
 });
