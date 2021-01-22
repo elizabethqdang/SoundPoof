0a644d9b163f8c34be38967b779670e38ec50dcd
elizabethqdang <elizabethqdang@gmail.com>
2021-01-21 18:53:06 -0800

    completed revamp and app works in development mode

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'aws-sdk-s3', require: false
 # gem 'aws-s3', :require => 'aws/s3'
 gem 'seed_dump'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2'
+gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "../reducers/root_reducer";
 
 const middlewares = [thunk];
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 if (process.env.NODE_ENV !== "production") {
-	// must use "reqire", import only6 allowed at top of file
+	// must use "require", import only allowed at top of file
 	const { logger } = require("redux-logger");
 	middlewares.push(logger);
 }
@@ -15,8 +17,14 @@ const configureStore = (preloadedState = {}) =>
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(...middlewares)
-    // applyMiddleware(thunk, logger)
+    // applyMiddleware(...middlewares),
+		// applyMiddleware(thunk, logger)
+		// composeEnhancers(
+		// 	applyMiddleware(...middlewares)
+		// ),
+		composeWithDevTools(
+			applyMiddleware(...middlewares)
+		)
   );
 
 export default configureStore;
